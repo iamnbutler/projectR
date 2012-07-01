38b111be7166f4ca609d608916a935fe76e1be3a
Nathan Butler <iamnbutler@gmail.com>
2012-07-01 18:53:21 -0400

    Development order, exceeded functions
    Added development order
    Extended song, note, score systems
    Added placeholder for currency system

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,30 @@
 
 /***
 *
+*  Function development order:
+*    1.0 Note System
+*        Song System
+*        Score System
+*        Control System
+*
+*    1.1 Item System
+*        Control Options
+*        General Options
+*
+*    ?.? Module System
+*        PV Playback
+*
+***/
+
+/***
+*
 *  Song System [SONG]
 *
 ***/
 
+var songTitle = ""
+var mode = 0;
+var difficulty = 0;
 var tempo = 0;
 
 /***
@@ -26,8 +46,28 @@ var noteEnd = [0, 0];
 var noteSpawnTime = 0;
 var notePressTime = 0;
 var noteAccuracy = 0;
+	
+	// WORST
+	noteAccLevel0 = 0;
 
-function calculateAccuracy() {
+	// SAD
+	noteAccLevel1 = 0;
+
+	// SAFE
+	noteAccLevel2 = 0;
+
+	// FINE
+	noteAccLevel3 = 0;
+
+	// COOL
+	noteAccLevel4 = 0;
+
+function createNote(noteStart, noteEnd, tempo) {
+
+}
+
+function destroyNote() {
+	noteAccuracy = (noteSpawnTime - notePressTime) / tempo;
 
 }
 
@@ -40,6 +80,13 @@ function calculateAccuracy() {
 // Total score points at the end of a song (0 - 999,999)
 var score = 0;
 
+	// Stage sub-scores
+	var stageScore = 0;
+	var comboBonus = 0;
+	var chanceScore = 0;
+
+var highScore = 0;
+
 // 0 = MISSxTAKE, 1 = STANDARD, 2 = GREAT, 3 = EXCELLENT, 4 = PERFECT
 var grade = 0;
 
@@ -48,7 +95,34 @@ var notesPassed = 0;
 var notePercentage = 0;
 
 // Calculate the percentage of notes hit, and assign grade
-function calculateScore() {
-	notePercentage = (notesPassed / notesTotal) * 100;
+function calculateScore(notesPassed, notesTotal) {
+	// Calculate the percentage of notes hit. This value will always be rounded down to the nearest percent.
+	notePercentage = (Math.floor(notesPassed / notesTotal) * 100);
 
+	if (notePercentage >= 85) {
+		grade = 1;
+	} else if (notePercentage >= 95) {
+		grade = 2;
+	} else if (notePercentage >= 97) {
+		grade = 3;
+	} else if (notePercentage = 100) {
+		grade = 4;
+	} else {
+		// Song failed, assign MISSxTAKE grade
+		grade = 0;
+	}
+
+	// Calculate total score
+	score = (stageScore + comboBonus + chanceScore)
+
+	// Compare score agains high score
+	if (score > highScore) {
+		highScore = score;
+	}
 }
+
+/***
+*
+*  Currency System [CURR]
+*
+***/
