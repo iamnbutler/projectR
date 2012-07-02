ff61459e056d561efbbb3ffba0faa9eab07958d6
Nathan Butler <iamnbutler@gmail.com>
2012-07-02 19:46:21 -0400

    Syncing to change parent

--- js/scripts.js
+++ js/scripts.js
@@ -125,45 +125,45 @@ $(document).ready(function(){
 				// Detect note type and create appropriate note and placeholder
 				if (noteType == 0) {
 					// Regular note
-					$('.note').addClass('reg');
+					$('#note-' + noteId).children('.note').addClass('reg');
 
 					// Spawn regular gray target
-					$('.note-target').addClass('reg-target');
+					$('#note-' + noteId).children('.note-target').addClass('reg-target');
 				} else if (noteType == 1) {
 					// Long Note
-					$('.note').addClass('long');
+					$('#note-' + noteId).children('.note').addClass('long');
 
 					// Spawn note target with border
-					$('.note-target').addClass('long-target');
+					$('#note-' + noteId).children('.note-target').addClass('long-target');
 
 					// Spawn long note trail
 
 					// Spawn second note at end of trail
 				} else {
 					// Arrow note
-					$(this).children('.note').addClass('arrow');
+					$('#note-' + noteId).children('.note').addClass('arrow');
 
 					// Spawn arrow target
-					$(this).children('.note-target').addClass('arrow-target');
+					$('#note-' + noteId).children('.note-target').addClass('arrow-target');
 				};
 
 				// Detect note button and assign background to button and target
 				if (noteButton == 0) {
 					// Triangle (up) note
-					$(this).children('.note').addClass('ui-note-triangle');
-					$(this).children('.note-target').addClass('ui-note-triangle-target');
+					$('#note-' + noteId).children('.note').addClass('ui-note-triangle');
+					$('#note-' + noteId).children('.note-target').addClass('ui-note-triangle-target');
 				} else if (noteButton == 1) {
 					// Circle (right) note
-					$('.note').addClass('ui-note-circle');
-					$('.note-target').addClass('ui-note-circle-target');
+					$('#note-' + noteId).children('.note').addClass('ui-note-circle');
+					$('#note-' + noteId).children('.note-target').addClass('ui-note-circle-target');
 				} else if (noteButton == 2) {
 					// X (down) note
-					$(this).children('.note').addClass('ui-note-x');
-					$(this).children('.note-target').addClass('ui-note-x-target');
+					$('#note-' + noteId).children('.note').addClass('ui-note-x');
+					$('#note-' + noteId).children('.note-target').addClass('ui-note-x-target');
 				} else {
 					// Square (left) note
-					$(this).children('.note').addClass('ui-note-square');
-					$(this).children('.note-target').addClass('ui-note-square-target');
+					$('#note-' + noteId).children('.note').addClass('ui-note-square');
+					$('#note-' + noteId).children('.note-target').addClass('ui-note-square-target');
 				};
 			
 				// Position the note and animate to the target using the tempo multiplied by a constant as animation speed
@@ -204,12 +204,11 @@ $(document).ready(function(){
 	};
 
 	// createNote variables: noteSpawnTime, noteType, noteButton, noteStart, noteEnd, tempo
-	createNote(1,0,1,[0,0],[39,24],127);
-	createNote(2,0,1,[0,0],[43,24],127);
-	createNote(3,0,1,[0,0],[47,24],127);
-	createNote(4,0,1,[0,0],[51,24],127);
-	createNote(5,0,1,[0,0],[55,24],127);
-	createNote(6,0,1,[0,0],[59,24],127);
+	createNote(1.5,0,1,[10,0],[15,66],136);
+	createNote(3.5,0,1,[25,100],[30,33],136);
+	createNote(5.5,0,1,[40,0],[45,66],136);
+	createNote(7.5,0,1,[50,100],[55,33],136);
+	createNote(9.5,0,1,[65,0],[70,66],136);
 
 	/***
 	*
